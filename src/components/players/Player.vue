150349274286f4672f0b2e84c28196015e285ccb
Abry <vega.antler@gmail.com>
2020-02-09 21:40:36 +0800

    added player images

--- src/components/players/Player.vue
+++ src/components/players/Player.vue
@@ -2,7 +2,7 @@
     <div class="col-sm-6 col-md-6">
         <div class="panel panel-success">
             <div class="panel-heading">
-                <h3 class="panel-title">
+                <h3 class="panel-title" :style="{backgroundImage: 'url(https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/'+ player.playerId +'.png)'}">
                     {{ player.firstName + ' ' + player.lastName }}
                     <small>(Rating: {{ player.rating  }})</small>
                 </h3>
@@ -16,3 +16,12 @@
         props: ['player']
     }
 </script>
+
+<style scoped>
+    .panel-title {
+        background-repeat: no-repeat;
+        background-position: center right;
+        padding: 20px;
+        background-size: contain;
+    }
+</style>
